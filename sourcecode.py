045176c4d890789dec7af05c9867244e1e50d645
Anjalichary12 <148260735+Anjalichary12@users.noreply.github.com>
2024-06-18 17:50:38 +0530

    Create sourcecode.py
    This is the python source code for wisecow application

---
+++ sourcecode.py
@@ -0,0 +1,14 @@
+ascii_art = r"""
+ ________________________________________
+/ Living your life is a task so           \
+| difficult, it has never been attempted |
+\ before.                                 /
+ ----------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+"""
+
+print(ascii_art)
